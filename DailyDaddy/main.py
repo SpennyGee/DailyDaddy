b8f344a23ed4b15e86322c8053f38e84d638c98e
SpennyGee <49570419+SpennyGee@users.noreply.github.com>
2020-10-01 13:24:58 -0700

    Update main.py

--- DailyDaddy/main.py
+++ DailyDaddy/main.py
@@ -5,13 +5,13 @@ GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""{
      "type": "service_account",
      "project_id": "triple-shadow-276319",
      "private_key_id": "1bfc89372e1fad527290eb947439cb35d468f261",
-     "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDh8qve2I4Yxp2m\n8+1UOsbcIUczoix0GWm9ewM1VYKSPvSYGVhb55CNpHduPpdqUbGzZEaDEX2BxiIb\nxbhF8YzVdGr4ac471KIXDQD15N5F7Cy9YC1fR7oosvDLgW7OUiNQRgrLL3xD90K3\nPwBJwKI4/ZsPHwTcrUWktC8wHAMlvvOI+u/84YZZUkuVU1AvCuuo5VeoA6x6BDFX\nWPzapHwMIEtpATMlt/7l1kuiX2yFH1rHjfXpqh/igGBBR12gncF9Yv3UsvGFQcan\nKlLQ9jkQ4QDh39+Zdk/EHd7FfRBjSwBEX2+ra+ti/eipobwKnGJuBEeK8o/dgbZV\nv8teeY5TAgMBAAECggEAEJjnW1WUACm2BfM86tpTpj+2WVdSpaECQ/FuN5o7uHg+\nz/dr2eBr0J1h5B7D6s+42nRSVUMEWAz3KQwZyhJN/2LV9fNFVRH8nO3b6E2/roPR\nRR30Z0XZD/x8NK67nK53uptSiqdgnPHj6QFyb3h8Uh+XwGe0Am1vkR0Ad5IiZw2S\n9UrLvdYwwDZizaGdJfvAsN9zXtB53TWG1c62uvijziSJTnASetLFJUlcBZAKZivG\nlz1BH1q1ol1BicuxhaKDjrux0wny5LVwH0fz7ArKE6uppFwrb42qTt16HlHsY0Iz\nVaCMaM6WeaWglf7MYUNhz7hAl9RVp+qSd40layjQAQKBgQD2G2/FB/R0sHqdR9Wi\npbY2aIHRuNDP3GvSSY8S37cgFy6Yi/EYOv5eGCsJLLqJgvYJHB/wuRLCDiulknRf\n0szJ8TOn5qWv9HLZf9ffiSqetpd9UBgvLGjDA1ygR/ou+5Qerjq/t7OYLgKvo9/t\nwVQUs1HK7/yJa753XZEkeTDQAQKBgQDrB8lMVDe/DHkkSS7DnERnv/QCKc7bp2Vv\nlxLRXCcKq9LIwgdfXeIB1ocsVNSayFv00PcQqN70RNadPcXzkjldXmAXOL2QK0Uu\nFlE+NxuUkFbwxHkhfW5yJvI14Y0YbFNRaqEerggTYcW1d2W8erJXFUKBc2KZAitC\n5eN7e0YeUwKBgQDbOx2Zmrh8qijPqon4VT23wejC9autPmEd9kDpQzhR8dSkNyUR\nvJuTHlxX0+xnSq3494DMY1D+ZLkkyQ25voKG8cJeMuwtjcgxT9zxV7MLpwPTsWGZ\nxCmeboQ4k2WeLKWJnxLebboiOfpSk9yKYsmPlx2pdZa0o7ILiuIcZ4jAAQKBgQDf\n9MZzLlkEeq1Az195VAG23ylzmkUb95Hn2Iw85VPqjjpHxOPjkwFWgQbDTn5ck4lU\nan7S6CezmZjrsr5az75L4y8rUY0IwMKmHOwDnd1xHm1J9d369Jv2uHfasfeSCz8F\neNbY9jqJxYPw3DocdMpVwtNhqQKODIwOriKrOpdhMwKBgQCwf8yWVdrS3DfngZIU\nTY+ABdwHeyOghzTCS83bcmvFOjHhsq+mUASeefjIaj9qXQo5MXTyzrY/dFMFZKuV\n76ojCdl4lGJGepU4khhhuW7V1Mt0UvDi4q+gRMXGgWvy/UGuevYj84bYU939ig7A\nTGFpAzMj1z93Vzkfb/7poorrYw==\n-----END PRIVATE KEY-----\n",
+     "private_key": "-----BEGIN PRIVATE KEY-----\
      "client_email": "starting-account-qspw946dixx1@triple-shadow-276319.iam.gserviceaccount.com",
      "client_id": "100175108747364243003",
      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
      "token_uri": "https://oauth2.googleapis.com/token",
      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-     "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/starting-account-qspw946dixx1%40triple-shadow-276319.iam.gserviceaccount.com"
+     "client_x509_cert_url": ****
    }"""
 
 class DailyDaddyApp(App):
